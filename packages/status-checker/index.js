8c0a61af23dd851b7de6ae03473bcf2ff9c5f20b
Kanushka Gayan <kanushkanet@gmail.com>
2024-01-23 14:52:02 +0530

    Refactor service status checking with async/await

--- packages/status-checker/index.js
+++ packages/status-checker/index.js
@@ -30,23 +30,39 @@ const devPublicServices = [
     },
 ];
 
+const delay = process.env.DELAY || 2000;
+
 function log(message, type = "INFO") {
     const dateTime = new Date().toISOString();
     console.log(`[${dateTime}] [${type}] ${message}`);
 }
 
-function checkServiceStatus(service) {
-    axios
-        .get(service.url + "/status")
-        .then((response) => {
-            log(`${service.name} is up: Response - ${JSON.stringify(response.data)}`);
-        })
-        .catch((error) => {
-            log(`Error accessing ${service.name}: ${error.message}`, "ERROR");
-        });
+async function checkServiceStatus(service) {
+    try {
+        const response = await axios.get(service.url + "/status");
+        log(`${service.name} is up: Response - ${JSON.stringify(response.data)}`);
+    } catch (error) {
+        log(`Error accessing ${service.name} from ${service.url}/status : ${error.message}`, "ERROR");
+    }
 }
 
-// Check the status of each service
-log(">>> Checking service statuses...");
-projectServices.forEach((service) => checkServiceStatus(service));
-devPublicServices.forEach((service) => checkServiceStatus(service));
+async function checkAllServiceStatuses() {
+    log("Starting status checks");
+    for (const service of projectServices) {
+        await checkServiceStatus(service);
+        await new Promise((resolve) => setTimeout(resolve, delay));
+    }
+
+    for (const service of devPublicServices) {
+        await checkServiceStatus(service);
+        await new Promise((resolve) => setTimeout(resolve, delay));
+    }
+}
+
+checkAllServiceStatuses()
+    .then(() => {
+        log("Status checks complete");
+    })
+    .catch((error) => {
+        log(`Error occurred during status checks: ${error.message}`, "ERROR");
+    });
